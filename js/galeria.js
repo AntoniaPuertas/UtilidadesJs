6f2f2023c1d4c42d3026eb8cd28e2da8a9080511
Totilla <tsm.antonia@gmail.com>
2024-08-07 12:42:37 +0200

    Update galeria.js

--- js/galeria.js
+++ js/galeria.js
@@ -1,18 +1,17 @@
 const imagenPrincipal = document.getElementById('imagenPrincipal');
 const miniaturas = document.querySelectorAll('.miniatura');
+// const miniaturas = document.getElementsByClassName('miniatura');
 
 const miniaturasArray = [
-    {imagen: "img/cebra.jpg", altImagen: "Cebra", imagenPeq: "img/miniaturas/cebra_peq.jpg"},
-    {imagen: "img/girafa.jpg", altImagen: "Girafa", imagenPeq: "img/miniaturas/girafa_peq.jpg"},
-    {imagen: "img/guepardo.jpg", altImagen: "Guepardo", imagenPeq: "img/miniaturas/guepardo_peq.jpg"},
-    {imagen: "img/leon.jpg", altImagen: "León", imagenPeq: "img/miniaturas/leon_peq.jpg"},
+    {imagen: "img/cebra.jpg", altImagen: "Cebra"},
+    {imagen: "img/girafa.jpg", altImagen: "Girafa"},
+    {imagen: "img/guepardo.jpg", altImagen: "Guepardo"},
+    {imagen: "img/leon.jpg", altImagen: "León"},
 ]
 
 miniaturas.forEach(miniatura => {
     miniatura.addEventListener('click', function(e) {
         let idImagen = e.currentTarget.id;
-        console.log("evento:", e.currentTarget);
-        console.log("id: ", idImagen);
         imagenPrincipal.src = miniaturasArray[idImagen].imagen;
         imagenPrincipal.alt = miniaturasArray[idImagen].altImagen;
     });
